2f7b7b906cbc35628055be0cb1c4100109b250af
Vaish26-J <56188118+Vaish26-J@users.noreply.github.com>
2023-12-11 12:58:32 +0530

    image url changes in script.js

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ let pokemons = [
    {
     key: "pikachu",
     title: "PIKACHU",
-    imgUrl: "/assets/pikachu.png",
+    imgUrl: "assets/pikachu.png",
     about: "When it is angered, it immediately discharges the energy stored in the pouches in its cheeks. When several of these Pokémon gather, their electricity can build and cause lightning storms.",
     type: "Electric",
     abilities: "Static"
@@ -31,7 +31,7 @@ let pokemons = [
    {
     key: "aipom",
     title: "AIPOM",
-    imgUrl: "/assets/aipom.png",
+    imgUrl: "assets/aipom.png",
     about: "As it did more and more with its tail, its hands became clumsy. It makes its nest high in the treetops. It searches for prey from the tops of trees. When it spots its favorite food, Bounsweet, Aipom gets excited and pounces.",
     type: "Normal",
     abilities: "Runaway, Pickup"
@@ -39,7 +39,7 @@ let pokemons = [
    {
     key: "bulbasaur",
     title: "BULBASAUR",
-    imgUrl: "/assets/bulbasaur.png",
+    imgUrl: "assets/bulbasaur.png",
     about: "There is a plant seed on its back right from the day this Pokémon is born. The seed slowly grows larger. While it is young, it uses the nutrients that are stored in the seed on its back in order to grow.",
     type: "Grass, Poison",
     abilities: "Overgrow"
@@ -47,7 +47,7 @@ let pokemons = [
    {
     key: "charmandar",
     title: "CHARMANDAR",
-    imgUrl: "/assets/charmandar.png",
+    imgUrl: "assets/charmandar.png",
     about: 'It has a preference for hot things. When it rains, steam is said to spout from the tip of its tail. From the time it is born, a flame burns at the tip of its tail. Its life would end if the flame were to go out.',
     type: "Fire",
     abilities: "Blaze"
@@ -55,7 +55,7 @@ let pokemons = [
    {
     key: "ditto",
     title: "DITTO",
-    imgUrl: "/assets/ditto.png",
+    imgUrl: "assets/ditto.png",
     about: "Its transformation ability is perfect. However, if made to laugh, it can’t maintain its disguise. It can freely recombine its own cellular structure to transform into other life-forms.",
     type: "Normal",
     abilities: "Limber"
@@ -63,7 +63,7 @@ let pokemons = [
    {
     key: "eevee",
     title: "EEVEE",
-    imgUrl: "/assets/eevee.png",
+    imgUrl: "assets/eevee.png",
     about: "Its ability to evolve into many forms allows it to adapt smoothly and perfectly to any environment. Its genetic code is irregular. It may mutate if it is exposed to radiation from element stones.",
     type: "Normal",
     abilities: "Runaway, Adaptability"
@@ -71,7 +71,7 @@ let pokemons = [
    {
     key: "jigglypuff",
     title: "JIGGLYPUFF",
-    imgUrl: "/assets/jigglypuff.png",
+    imgUrl: "assets/jigglypuff.png",
     about: "When its huge eyes waver, it sings a mysteriously soothing melody that lulls its enemies to sleep. If it inflates to sing a lullaby, it can perform longer and cause sure drowsiness in its audience.",
     type: "Normal, Fairy",
     abilities: "Cute Charm, Competitive"
@@ -79,7 +79,7 @@ let pokemons = [
    {
     key: "lucario",
     title: "LUCARIO",
-    imgUrl: "/assets/lucario.png",
+    imgUrl: "assets/lucario.png",
     about: "It’s said that no foe can remain invisible to Lucario, since it can detect auras—even those of foes it could not otherwise see. A well-trained one can use its aura to identify and take in the feelings of creatures over half a mile away.",
     type: "Fighting, Steel",
     abilities: "Inner Focus, Stead Fast"
@@ -87,7 +87,7 @@ let pokemons = [
    {
     key: "meowth",
     title: "MEOWTH",
-    imgUrl: "/assets/meowth.png",
+    imgUrl: "assets/meowth.png",
     about: "All it does is sleep during the daytime. At night, it patrols its territory with its eyes aglow. It loves things that sparkle. When it sees a shiny object, the gold coin on its head shines, too.",
     type: "Normal",
     abilities: "Pickup, Technician"
@@ -95,7 +95,7 @@ let pokemons = [
    {
     key: "mewtwo",
     title: "MEWTWO",
-    imgUrl: "/assets/mewtwo.png",
+    imgUrl: "assets/mewtwo.png",
     about: "Its DNA is almost the same as Mew’s. However, its size and disposition are vastly different.",
     type: "Psychic",
     abilities: "Pressure"
@@ -103,7 +103,7 @@ let pokemons = [
    {
     key: "mr.mime",
     title: "Mr. MIME",
-    imgUrl: "/assets/Mr. Mime.png",
+    imgUrl: "assets/Mr. Mime.png",
     about:  "It is a pantomime expert that can create invisible but solid walls using miming gestures. Emanations from its fingertips solidify the air into invisible walls that repel even harsh attacks.",
     type: "Psychic, Fairy",
     abilities: "Soundproof, Filter"
@@ -111,7 +111,7 @@ let pokemons = [
    {
     key: "pidgey",
     title: "PIDGEY",
-    imgUrl: "/assets/pidgey.png",
+    imgUrl: "assets/pidgey.png",
     about: "Very docile. If attacked, it will often kick up sand to protect itself rather than fight back.",
     type: "Normal, Flying",
     abilities: "Keen eye, Tangled Feet"
@@ -119,7 +119,7 @@ let pokemons = [
    {
     key: "psyduck",
     title: "PSYDUCK",
-    imgUrl: "/assets/psyduck.png",
+    imgUrl: "assets/psyduck.png",
     about: "It is constantly wracked by a headache. When the headache turns intense, it begins using mysterious powers. If its chronic headache peaks, it may exhibit odd powers. It seems unable to recall such an episode.",
     type: "Water",
     abilities: "Damp, Cloud Nine"
@@ -127,7 +127,7 @@ let pokemons = [
    {
     key: "snorlax",
     title: "SNORLAX",
-    imgUrl: "/assets/snorlax.png",
+    imgUrl: "assets/snorlax.png",
     about: "Its stomach can digest any kind of food, even if it happens to be moldy or rotten. It stops eating only to sleep. It doesn’t feel full unless it eats nearly 900 pounds a day.",
     type: 'Normal',
     abilities: "Thick Fat, Immunity"
@@ -135,7 +135,7 @@ let pokemons = [
    {
     key: "squirtle",
     title: "SQUIRTLE",
-    imgUrl: "/assets/squirtle.png",
+    imgUrl: "assets/squirtle.png",
     about: "When it retracts its long neck into its shell, it squirts out water with vigorous force. When it retracts its long neck into its shell, it squirts out water with vigorous force.",
     type: "Water",
     abilities: "Torrent"
@@ -143,7 +143,7 @@ let pokemons = [
    {
     key: "togepi",
     title: "TOGEPI",
-    imgUrl: "/assets/togepi.png",
+    imgUrl: "assets/togepi.png",
     about: "The shell seems to be filled with joy. It is said that it will share good luck when treated kindly. It is considered to be a symbol of good luck. Its shell is said to be filled with happiness.",
     type: "Fairy",
     abilities: "Serene Grace, Hustle"
